1b27fad585aade9ca765578b52b87e27b256659c
Sammy atman <askutubeai@gmail.com>
2025-08-23 10:47:14 -0700

    Updates to train.py

--- train.py
+++ train.py
@@ -6,17 +6,23 @@ from lerobot.common.policies.factory import make_policy_config
 import os
 from pathlib import Path
 import wandb
+import torch
 
-def start_training(repo_id, root=None, output_dir='./checkpoints/baseline', policy_name='act', job_name='', **kwargs):
+def start_training(repo_id, root=None, output_dir=None, policy_name='act', job_name='', **kwargs):
     dataset = DatasetConfig(repo_id=repo_id, root=root)
     policy = make_policy_config(policy_name)
     #policy.chunk_size = 1
     #policy.n_action_steps = 1
 
     device = 'mps'
-    output_dir = Path(output_dir)
+
     dataset_name = repo_id.replace('/', '_')
     full_job_name = f"{job_name or 'train'}_{policy_name}_{dataset_name}"
+
+    if not output_dir:
+        output_dir = f'./checkpoints/{full_job_name}'
+    
+    output_dir = Path(output_dir)
     wandb_config = WandBConfig(enable=True)
 
     train_config = TrainPipelineConfig(
@@ -25,11 +31,13 @@ def start_training(repo_id, root=None, output_dir='./checkpoints/baseline', poli
         wandb=wandb_config,
         output_dir=output_dir,
         job_name=full_job_name,
-        batch_size=8,
-        steps=100,
-        log_freq=1,
+        batch_size=4,
+        steps=10000,
+        log_freq=200,
+        eval_freq=200,
+        #eval_holdout_split="0:5",
         #resume=True,
-        num_workers=0)
+        num_workers=4)
 
     lerobot_train.train(train_config)
     wandb.finish()
