f30c38dbcad0de74ea533d6241073da4786c84f2
Sammy atman <askutubeai@gmail.com>
2025-09-09 04:19:06 +0000

    Added no grad, wandb logging and move to device in evaluation

--- evaluation.py
+++ evaluation.py
@@ -3,6 +3,7 @@ from contextlib import nullcontext
 from lerobot.policies.act.modeling_act import ACTPolicy
 from torch.utils.data import DataLoader, default_collate
 import torch
+import wandb
 
 def get_eval_episodes(good_episodes, eval_percentage=1.0):
     num_episodes = int(eval_percentage * len(good_episodes))
@@ -11,6 +12,18 @@ def get_eval_episodes(good_episodes, eval_percentage=1.0):
     # For filtered, last few episodes are eval
     filtered_eval_episodes = list(range(num_episodes))
     return baseline_eval_episodes, filtered_eval_episodes
+
+def move_to_device(data, device):
+    if isinstance(data, dict):
+        return {k: move_to_device(v, device) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [move_to_device(v, device) for v in data]
+    elif isinstance(data, tuple):
+        return tuple(move_to_device(v, device) for v in data)
+    elif isinstance(data, torch.Tensor):
+        return data.to(device)
+    else:
+        return data
 
 def run_eval(policy_path, repo_id, wandb_id, episodes, use_amp=False, root=None):
     wandb.init(project="lerobot", id=wandb_id, resume="must")
@@ -26,20 +39,21 @@ def run_eval(policy_path, repo_id, wandb_id, episodes, use_amp=False, root=None)
     )
 
     policy = ACTPolicy.from_pretrained(policy_path)
-    policy = policy.to('cpu')
     policy.config.use_vae = False
     policy.training = False
     policy.eval()
     total_loss = 0
-    with torch.autocast(device_type=device.type) if use_amp else nullcontext():
+    with torch.no_grad(), torch.autocast(device_type=device.type) if use_amp else nullcontext():
         for i, batch in enumerate(loader):
             batch['action_is_pad'] = torch.zeros(policy.config.n_action_steps).bool()
             #print(batch.keys())
             print(f'Evaluating batch: {i}/{len(loader)}')
             #batch = batch.to('mps')
+            batch = move_to_device(batch, policy.config.device)
             loss, output_dict = policy.forward(batch)
             total_loss += loss
-    print(f'Mean loss: {total_loss/len(loader)}')
+    mean_loss = total_loss / len(loader)
+    print(f'Mean loss: {mean_loss}')
     # TODO: Add wandb logging
     wandb.log({"eval/mean_loss": mean_loss})
     wandb.finish()
